95253ba4180d69caa5c13512a57aed72761006da
Elaina <marvintrupp@web.de>
2021-07-25 15:35:21 +0200

    Update user.go

--- user.go
+++ user.go
@@ -7,7 +7,7 @@ import (
 	"github.com/elainaaa/go-quaverapi/structs"
 )
 
-func GetUser(id int) (*structs.User, error) {
+func GetUser(id int) (*structs.UserData, error) {
 	rawData, err := ApiCall(fmt.Sprintf("/users/full/%d", id))
 	if err != nil {
 		return nil, err
@@ -22,7 +22,7 @@ func GetUser(id int) (*structs.User, error) {
 	return &user.User, nil
 }
 
-func SearchUsers(username string) (*structs.Users, error) {
+func SearchUsers(username string) (*structs.UserInfo, error) {
 	rawData, err := ApiCall(fmt.Sprintf("/users/search/%s", username))
 	if err != nil {
 		return nil, err
