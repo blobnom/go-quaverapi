80e4543b2483007a69a07855dde8ceb6572ed375
Elaina <marvintrupp@web.de>
2021-07-28 01:16:22 +0200

    2/3

--- map.go
+++ map.go
@@ -7,13 +7,13 @@ import (
 	"github.com/elainaaa/go-quaverapi/structs"
 )
 
-func GetMapset(id int) (*structs.Mapset, error) {
+func GetMapset(id int) (*structs.Mapsets, error) {
 	rawData, err := ApiCall(fmt.Sprintf("/mapsets/%d", id))
 	if err != nil {
 		return nil, err
 	}
 
-	mapset := structs.Mapset{}
+	mapset := structs.Mapsets{}
 	err = json.Unmarshal(rawData, &mapset)
 	if err != nil {
 		return nil, err
@@ -22,13 +22,13 @@ func GetMapset(id int) (*structs.Mapset, error) {
 	return &mapset, nil
 }
 
-func GetMap(id int) (*structs.Map, error) {
+func GetMap(id int) (*structs.Maps, error) {
 	rawData, err := ApiCall(fmt.Sprintf("/maps/%d", id))
 	if err != nil {
 		return nil, err
 	}
 
-	maps := structs.Map{}
+	maps := structs.Maps{}
 	err = json.Unmarshal(rawData, &maps)
 	if err != nil {
 		return nil, err
