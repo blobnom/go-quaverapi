1626ed8d12c97b760ca806f799b5f24932dd1f82
Elaina <marvintrupp@web.de>
2021-08-02 01:48:30 +0200

    Create server.go

---
+++ server.go
@@ -0,0 +1,21 @@
+package quaverapi
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func GetServerStats() (ServerStats, error) {
+	rawData, err := ApiCall("/stats")
+	if err != nil {
+		return Server{}, err
+	}
+
+	server := Server{}
+	err = json.Unmarshal(rawData, &server)
+	if err != nil {
+		return UserData{}, err
+	}
+
+	return server.Stats, nil
+}
